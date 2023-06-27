a238c1e57d4780abff0376d26793b8d2b27bfdf6
Lucas Samuel <lucass.m1jk@gmail.com>
2023-06-27 16:37:39 -0300

    first commit

---
+++ src/Exercicio1.java
@@ -0,0 +1,22 @@
+public class Exercicio1{
+    public static void main (String[] args){
+
+
+        int n1 = 30;
+        int n2 = 10;
+        int result = n1 + n2;
+
+        int n3 = -30;
+        int n4 = 10;
+        int result1 = n3 + n4;
+
+        int n5 = 0;
+        int n6 = 0;
+        int result2 = n5 + n6;
+
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+
+    }
+}
