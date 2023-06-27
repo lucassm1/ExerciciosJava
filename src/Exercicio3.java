029163eec8033212ae863af6bc41e5f9d6d27ea8
Lucas Samuel <lucass.m1jk@gmail.com>
2023-06-27 17:13:24 -0300

    Adição do terceiro exercício feito

--- src/Exercicio3.java
+++ src/Exercicio3.java
@@ -1,2 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exercicio3 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int EmployeeNumber, HoursWorked;
+        double ValuePerHour, Wage;
+
+        System.out.println("Enter your employee number:");
+            EmployeeNumber = sc.nextInt();
+        System.out.println("Enter the number of hours worked:");
+            HoursWorked = sc.nextInt();
+
+        Locale.setDefault(Locale.US);
+        /*Use "," instead of "." Example: 10,50*/
+        System.out.println("How much do you earn per hour worked: ");
+            ValuePerHour = sc.nextDouble();
+            Wage = ValuePerHour * HoursWorked;
+
+        System.out.printf("Employee number: %d%nWage: $%.2f", EmployeeNumber, Wage);
+
+        sc.close();
+    }
 }
+s
